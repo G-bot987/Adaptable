944e262294f43c1ff3d72f9bf55469d93ba7082d
gbot987 <ghobs987@gmail.com>
2023-05-14 22:32:44 +0100

    header scss and header comp

--- adaptable/src/app/mainComponents/Header/Header.tsx
+++ adaptable/src/app/mainComponents/Header/Header.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import Image from "next/image";
+import Navbar from './navbar/Navbar';
+import NewBooking from './newBooking/NewBooking';
+import { StaticImageData } from "next/image"
+import { newBookingInterface } from '../../../../interfaces/headerInterfaces/header.interfaces';
 import './Header.scss';
 
-export default function Header() {
+
+interface headerDataInterface {
+    headerData: {
+        logo: logoInterface
+        newBooking: newBookingInterface
+        navbar: string[]
+    }
+}
+
+interface logoInterface {
+    image: StaticImageData,
+    alt: string,
+}
+
+
+
+export default function Header(props: headerDataInterface) {
+    const { newBooking, navbar } = props.headerData
+    const { image, alt } = props.headerData.logo
     return (
-        <div className='header'>Header</div>
+        <section className='header'>
+            <section className='header__upper'>
+                <div className='header__upper__logo'>
+                    <Image src={image} alt={alt} />
+                </div>
+                <Navbar {...navbar} />
+                <NewBooking {...newBooking} />
+            </section>
+        </section>
     )
 }
