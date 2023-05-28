a1fe9ef489a0451278d4644676bbc0a0c5687126
gbot987 <ghobs987@gmail.com>
2023-05-28 21:52:00 +0100

    new interface for useState and conditional classes on navbar

--- adaptable/pages/mainComponents/Header/navbar/Navbar.tsx
+++ adaptable/pages/mainComponents/Header/navbar/Navbar.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Navbar.module.scss';
 
+interface navbarDataInterface {
+  navbar: string[]
+  navSelected: string
+  SetNavSelected: (value: string) => void;
+}
 
-export default function Navbar(props: string[]) {
+export default function Navbar(props: navbarDataInterface) {
+
+  const { navbar, navSelected, SetNavSelected } = props
+
+  useEffect(() => {
+  }, [navSelected])
+
   return (
     <ul className={styles.navbar}>
-      {Object.keys(props).map((option: any, index: number) => (
-        <li className={styles.navbar__btnWrapper} key={index}>
-          <button className={styles.navbar__btnWrapper__btn}>
-            {props[option]}
+      {navbar?.map((option: any, index: number) => (
+        <li className={`${styles.navbar__btnWrapper} ${navSelected === option ? styles['navbar__btnWrapper--selected'] : ''}`} key={index}>
+          <button onClick={() => { SetNavSelected(`${option}`) }} className={`${styles.navbar__btnWrapper__btn} ${navSelected !== option ? styles['navbar__btnWrapper__btn--notSelected'] : ''}`}>
+            {option}
           </button>
         </li>
       ))}
